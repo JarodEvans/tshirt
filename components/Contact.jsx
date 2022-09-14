6d2c378824d2e3b2ad1efcc9cbcdc845f940d6b7
blockstar-jarod\Jarod Evans <jarod@blockstars.com.au>
2022-09-14 10:18:23 +1000

    Contacts setup. Generic product page setup.

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 export default function Contact() {
     return (
-        <div>
-            Hello Contacts
+        <div className='max-w-[1240px] m-auto p-4 h-full'>
+            <h1 className='text-2xl font-bold text-center p-4'> Enter your details and send us a message! </h1>
+            <form className='max-w-[600px] m-auto'>
+                <div className='grid grid-cols-2 gap-2'>
+                    <input className='border shadow-lg p-3' type='text' placeholder='Name' />
+                    <input className='border shadow-lg p-3' type='email' placeholder='Email' />
+                </div>
+                <input className='border shadow-lg p-3 w-full my-2' type='text' placeholder='Subject' />
+                <textarea className='border shadow-lg p-3 w-full' name='' id='' cols='30' rows='10' placeholder='Message'></textarea>
+                <button className='border shadow-lg p-3 w-full mt-2'>Submit</button>
+            </form>
+            <div className='max-w-[1240px] m-auto p-4 h-full'>
+                <form className='max-w-[600px] m-auto'>
+                <h2 className='text-2xl font-bold text-center p-4'> or Contact us directly </h2>
+                <h3 className='text-1xl font-bold text-left p-4'>Phone number: 555 555 555</h3>
+                <h3 className='text-1xl font-bold text-left p-4'>Email: example@email.com</h3>
+                <p className='p-4'>Call us anytime about our suppliers, prices, locations etc.</p>
+                </form>
+            </div>
         </div>
     );
   }
--- components/Tshirts.jsx
+++ components/Tshirts.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Welcome from '../components/Welcome';
-import jordans from '../assets/jordans.avif';
 import whiteShirt from '../assets/whiteshirt.jpg';
 import greenShirt from '../assets/greenShirt.jpg';
 import blackShirt from '../assets/blackShirt.jpg';
@@ -13,7 +9,7 @@ const products = [
     {
       id: 1,
       name: 'White Shirt',
-      href: "https://images.unsplash.com/photo-1522706604291-210a56c3b376?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      href: '/product',
       price: '$48',
       imgSrc: whiteShirt,
       imageAlt: 'White Shirt',
@@ -21,7 +17,7 @@ const products = [
     {
       id: 2,
       name: 'Green Shirt',
-      href: 'https://images.unsplash.com/photo-1489913590284-9269438bb411?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+      href: '/product',
       price: '$35',
       imgSrc: greenShirt,
       imageAlt: 'Green Shirt',
@@ -29,7 +25,7 @@ const products = [
     {
       id: 3,
       name: 'Black Shirt',
-      href: 'https://images.unsplash.com/photo-1527534085786-f44cde0659a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80',
+      href: '/product',
       price: '$89',
       imgSrc: blackShirt,
       imageAlt: 'Black Shirt',
@@ -37,14 +33,16 @@ const products = [
     {
       id: 4,
       name: 'Dragon Shirt - Crop',
-      href: "https://images.unsplash.com/photo-1519722417352-7d6959729417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      href: '/product',
       price: '$35',
       imgSrc: dragonShirt,
       imageAlt: 'Dragon Shirt',
     },
   ]
 
+
 export default function Tshirt() {
+
     return (
       <div>
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -52,7 +50,10 @@ export default function Tshirt() {
 
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
-              <a key={product.id} href={product.href} className="group">
+              <a key={product.id}
+               href={product.href}
+                className="group"
+                >
                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                   <Image
                     src={product.imgSrc}
